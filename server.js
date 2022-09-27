f1bd7e2b1d384e5c0549caf9d6efdb51ed5ca1d5
Jeff McCracken <jjmccrac@gmail.com>
2022-09-27 11:40:27 -0700

    Part 2 fixed typos

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.use(express.static(__dirname + '/public'));
 
 app.use((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Cnotent-Type', 'text/html');
+    res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express server</h1></body></html>');
 });
 
 app.listen(port, hostname, () => {
-    console.log(`Server running at attp://${hostname}:${port}`);
+    console.log(`Server running at http://${hostname}:${port}`);
 });
